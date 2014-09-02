3a566e31a18537e1109abdb9ea223e55c7645ed9
btmcintyre <brian.mcintyre@sage.com>
2014-09-02 16:15:47 +0100

    Some small changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,8 +7,18 @@ describe "Static pages" do
 
   describe "Home page" do
     before { visit root_path }
-
-    it { should have_content('Golf Statistics')}
+		it { should have_selector('h1', text: 'Golf Statistics') }
     it { should have_title(full_title('Home'))}
   end
+
+	it "should have the right links on the layout" do
+	  visit root_path
+	  expect(page).to have_title(full_title('Home'))
+	  click_link "Home"
+	  expect(page).to have_title(full_title('Home'))
+	  click_link "Sign up now!"
+	  expect(page).to have_title(full_title('Sign up'))
+	  click_link "Golf Statistics"
+	  expect(page).to have_title(full_title('Home'))
+	end
 end
