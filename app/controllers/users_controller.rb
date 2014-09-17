fbb5e3266b25663f6801fa12d97acb4382f90ff6
btmcintyre <brian.mcintyre@sage.com>
2014-09-17 16:15:21 +0100

    Finish sigh in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,19 @@
 class UsersController < ApplicationController
-
-	def show
+  
+ 	def show
     @user = User.find(params[:id])
   end
   
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-    	 flash[:success] = "Welcome to the Golf Statistics Application!"
-       redirect_to @user
+      sign_in @user
+    	flash[:success] = "Welcome to the Golf Statistics Application!"
+      redirect_to @user
     else
       render 'new'
     end
