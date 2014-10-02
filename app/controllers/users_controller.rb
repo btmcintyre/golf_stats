1127cee94bc904d8273e06a4b6db666297749840
btmcintyre <brian.mcintyre@sage.com>
2014-10-02 14:36:14 +0100

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
-  
- 	def show
+  before_action :signed_in_user,  only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,    only: [:edit, :update]
+  before_action :admin_user,      only: :destroy
+  before_action :signed_out_user, only: [:new, :create]
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 10)
+  end
+
+  def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
@@ -12,17 +20,59 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-    	flash[:success] = "Welcome to the Golf Statistics Application!"
+      flash[:success] = "Welcome to the Golf Statistics Application!"
       redirect_to @user
     else
       render 'new'
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    unless current_user?(user)
+      user.destroy
+      flash[:success] = "User deleted."
+    end
+    redirect_to users_url
+  end
+
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
+  # Before filters
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
     end
+  end
+
+  def signed_out_user
+     redirect_to(root_url, notice: "You are already a User - Please sign in.") unless not signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Don Kennedy"
-    email    "donkennedy@example.com"
-    password "password"
-    password_confirmation "password"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@gmail.com"}
+    password "123456"
+    password_confirmation "123456"
+
+    factory :admin do
+      admin true
+    end
   end
 end
